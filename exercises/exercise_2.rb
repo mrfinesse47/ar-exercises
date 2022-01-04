36c7b3641a267751fc1e73fca36dfab0851724e9
Kevin Mason <krmason2020@gmail.com>
2022-01-04 00:12:48 +0000

    completed second exercise

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -6,6 +6,12 @@ puts "----------"
 
 # Your code goes here ...
 
-# david = User.find_by(name: 'David')
+# Exercise 2: Update the first store
+# Load the first store (with id = 1) from the database and assign it to an instance variable @store1.
+# Load the second store from the database and assign it to @store2.
+# Update the first store (@store1) instance in the database. (Change its name or something.)
 
-
+@store1 = Store.find_by(id:1)
+@store2 = Store.find_by(id:2)
+@store1.name = "Terrace"
+@store1.save
