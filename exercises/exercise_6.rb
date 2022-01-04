5b465a3ae3cd08216307bf8b6abab976587d11a7
Kevin Mason <krmason2020@gmail.com>
2022-01-04 01:39:50 +0000

    completed ex6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -22,3 +22,9 @@ puts "----------"
 # Add some data into employees. Here's an example of one (note how it differs from how you create stores): @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
 # Go ahead and create some more employees using the create method. You can do this by making multiple calls to create (like you have before.) No need to assign the employees to variables though.
 # Create them through the @store# instance variables that you defined in previous exercises. Create a bunch under @store1 (Burnaby) and @store2 (Richmond). Eg: @store1.employees.create(...).
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Kevin", last_name: "Mason", hourly_rate: 1000060)
+@store5 = Store.find_by(id:5)
+@store5.employees.create(first_name: "Robert", last_name: "Mason", hourly_rate: 10)
+@store5.employees.create(first_name: "Natalie", last_name: "Mason", hourly_rate: 11)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,14 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+# Exercise 7: Validations for both models
+# Add validations to two models to enforce the following business rules:
+# Employees must always have a first name present
+# Employees must always have a last name present
+# Employees have a hourly_rate that is a number (integer) between 40 and 200
+# Employees must always have a store that they belong to (can't have an employee that is not assigned a store)
+# Stores must always have a name that is a minimum of 3 characters
+# Stores have an annual_revenue that is a number (integer) that must be 0 or more
+
+
